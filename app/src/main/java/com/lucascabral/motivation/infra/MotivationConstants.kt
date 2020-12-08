291a5fad7df770bdaf549748c6eb6da5a0812590
lucascabraldeve <lucas_cabral.deve@outlook.com>
2020-12-08 12:04:53 -0300

    Adicionado evento de click e frases para o app.

--- app/src/main/java/com/lucascabral/motivation/infra/MotivationConstants.kt
+++ app/src/main/java/com/lucascabral/motivation/infra/MotivationConstants.kt
@@ -5,4 +5,11 @@ class MotivationConstants private constructor(){
     object KEY {
         const val PERSON_NAME = "name"
     }
+
+    object PHRASEFILTER {
+
+        val ALL = 1
+        val HAPPY = 2
+        val MORNING = 3
+    }
 }
--- app/src/main/java/com/lucascabral/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/lucascabral/motivation/ui/MainActivity.kt
@@ -1,22 +1,79 @@
 package com.lucascabral.motivation.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.lucascabral.motivation.R
 import com.lucascabral.motivation.infra.MotivationConstants
 import com.lucascabral.motivation.infra.SecurityPreferences
+import com.lucascabral.motivation.repository.Mock
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mSecurityPreferences: SecurityPreferences
+    private var mPhraseFilter: Int = MotivationConstants.PHRASEFILTER.ALL
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mSecurityPreferences = SecurityPreferences(this)
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+            //Lógica inicial
+        mainAllImageView.setColorFilter(resources.getColor(R.color.colorAccent))
+        handleNewPhrase()
 
-        mainNameTextView.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+        mSecurityPreferences = SecurityPreferences(this)
+        mainNameTextView.text = "Olá, ${mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)}"
+
+        mainNewPhraseButton.setOnClickListener(this)
+        mainAllImageView.setOnClickListener(this)
+        mainHappyImageView.setOnClickListener(this)
+        mainMorningImageView.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+
+        val id = view.id
+
+        val listFilter =
+            listOf(R.id.mainAllImageView, R.id.mainHappyImageView, R.id.mainMorningImageView)
+
+        if (id == R.id.mainNewPhraseButton) {
+            handleNewPhrase()
+        } else if (id in listFilter) {
+            handleFilter(id)
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+
+        mainAllImageView.setColorFilter(resources.getColor(R.color.white))
+        mainHappyImageView.setColorFilter(resources.getColor(R.color.white))
+        mainMorningImageView.setColorFilter(resources.getColor(R.color.white))
+
+        when (id) {
+            R.id.mainAllImageView -> {
+                mainAllImageView.setColorFilter(resources.getColor(R.color.colorAccent))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.ALL
+            }
+            R.id.mainHappyImageView -> {
+                mainHappyImageView.setColorFilter(resources.getColor(R.color.colorAccent))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.HAPPY
+            }
+            R.id.mainMorningImageView -> {
+                mainMorningImageView.setColorFilter(resources.getColor(R.color.colorAccent))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.MORNING
+            }
+        }
+    }
+
+    private fun handleNewPhrase() {
+
+        mainPhraseTextView.text = Mock().getPhrase(mPhraseFilter)
     }
 }
